1b372d15c35b7f46a91b53f721375c4bb6f6cd33
MeghanaYerninti <meghana.yerninti@sailssoftware.com>
2026-08-13 15:21:14 +0530

    updatde conditional rendering

--- src/ConditionalRendering.jsx
+++ src/ConditionalRendering.jsx
@@ -1,39 +1,36 @@
-import {useState} from "react";
+import { useState } from "react";
 
 function App() {
-    const [btn, setBtn] = useState(false);
-    const [todo, setTodo] = useState([
-        {id: 1, title: "Learn React"},
-        {id: 2, title: "Learn Redux"},
-        {id: 3, title: "Learn React Router"},
-    ]);
+  const [btn, setBtn] = useState(false);
+  const [todo, setTodo] = useState([
+    { id: 1, title: "Learn React" },
+    { id: 2, title: "Learn Redux" },
+    { id: 3, title: "Learn React Router" },
+  ]);
 
-    const Todo = ({todo}) => {
-        return (
-            <>
-            <div>
+  const greet = () => {
+    alert("Hello, welcome to React!");
+  }
+
+  return (
+    <>
+      {/* {btn? <button> Button</button> : ""} */}
+      <br />
+      {btn && <button onClick={greet}>Button</button>}
+      <br />
+      <button onClick={() => setBtn(!btn)}>Show/Hide Todos</button>{" "}
+      {/* toggle button to show and hide the todos */}
+      {btn &&
+        todo.map((todo) => {
+          return (
+            <div key={todo.id}>
                 <p className="todo">{todo.id}</p>
                 <p className="todo">{todo.title}</p>
-            </div>
-            </>
-        )
-    }
-
-    return (
-        <>
-            {/* {btn? <button> Button</button> : ""} */}
-            <br/>
-            {
-                btn && <button>Button</button>
-            }
-            <br/>
-            <button onClick={() => setBtn(!btn)}>Show/Hide Todos</button> {/* toggle button to show and hide the todos */}
-
-            {btn && todo.map((todo) => {
-                return (<Todo key={todo.id} todo={todo}/>)
-            })}
-        </>
-    )
+              </div>
+          );
+        })}
+    </>
+  );
 }
 
 export default App;
