cb935186e3b206653097c4ad56771fff6d8f30eb
MeghanaYerninti <meghana.yerninti@sailssoftware.com>
2026-08-19 14:59:54 +0530

    function without memo hook

--- src/UseMemoHook.jsx
+++ src/UseMemoHook.jsx
@@ -1,38 +1,19 @@
 import React from "react";
 import { useState, useMemo } from "react";
+import Navbar from "./componnet/Navbar";
 
-const nums = new Array(30_000_000).fill(0).map((_, i) => {
-  return {
-    index: i,
-    isMagical: i === 29_000_000,
-  };
-});
 const UseMemoHook = () => {
-  const [count, setCount] = useState(0);
-  const [numbers, setNumbers] = useState(nums);
-  // const magical numbers.find(item=>item.isMagical== true) // Expensive Computation
-  const magical = useMemo(
-    () => numbers.find((item) => item.isMagical === true), // our calculation
-    [numbers], // on change of which value we have to do the above calculation
-  );
-  return (
+  const [adjective, setAdjective] = useState("good")
+  const [count, setCount] = useState(0)
+
+  console.log("Main")
+  console.log(count)
+  return(
     <>
-      <span>Magical number is {magical.index}</span> <br/>
-      <button onClick={() => {
-        setCount(count + 1);
-        if(count==10){
-            setNumbers(new Array(10_000_000).fill(0).map((_,i) => { // we are only recalculating when number changed
-                return{
-                    index : i,
-                    isMagical: i===9_000_000
-            }
-            }))
-        }
-      }}>
-        Count is {count}
-      </button>
+    <button onClick={()=>{setCount(prev => prev+1)}}>Increase Count : {count}</button>
+    <Navbar adjective={adjective}/>
     </>
-  );
+  )
 };
 
 export default UseMemoHook;
---
+++ src/componnet/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { memo } from "react";
+
+const Navbar = ({adjective})=> {
+    console.log("Navbar")
+    return(
+        <p>This is {adjective} Navbar</p>
+    )
+};
+
+export default Navbar;
