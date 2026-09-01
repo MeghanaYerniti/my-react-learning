f72405f0d4665a385101504f1fd81d0e846cf582
MeghanaYerninti <meghana.yerninti@sailssoftware.com>
2026-09-01 15:38:27 +0530

    displaying in UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
+import React from "react";
 import { useState, useEffect } from "react";
 import getData from "./component/data.jsx";
 
 function App() {
-    // const[data, setData] = useState(null);
+    const[data, setData] = useState(null);
 
     useEffect(() => {
         getData().then((data) => {
             console.log(data);
-            // setData(data);
+            setData(data);
         });
     }, []);
 
     return(
         <>
+        {
+            data ? data.map((item) => {
+                <li>{item.title}</li>
+            }) : null
+        }
         </>
     )
 }
