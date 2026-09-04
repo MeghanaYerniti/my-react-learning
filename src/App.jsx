3bdca4a99ed49f4103c6c822389cdcf10c1788f8
MeghanaYerninti <meghana.yerninti@sailssoftware.com>
2026-09-04 16:53:16 +0530

    axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,27 @@
+import { useState, useEffect } from 'react';
+import getData from './component/data.jsx';
+
 function App() {
+
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        getData().then((data) => {
+            setData(data);
+        });
+    }, []);
+
+    console.log(data);
+
     return (
         <>
+            {/* <ul> */}
+                {
+                    data ? data.map((item) => (
+                        <li key={item.id}>{item.title}</li>
+                    )) : null
+                }
+            {/* </ul> */}
         </>
     )
 }
